60a71b619646883d866c4df76c4ad39529854a4f
ACCISA <aniss.chalah@gmail.com>
2023-12-29 13:52:02 -0500

    update

--- client/src/forms/AddVM.jsx
+++ client/src/forms/AddVM.jsx
@@ -14,44 +14,44 @@ export default function AddVM() {
     const handleAddVM = async (ev) => {
         ev.preventDefault();
         axios.post("http://localhost:8081/add", {
-          "name":vm_name,
-          "path":vm_path,
-          "ip":vm_ip
+            "name": vm_name,
+            "path": vm_path,
+            "ip": vm_ip
         })
-    
-      }
 
-    
+    }
+
+
     return (
         <div className="create">
-            <h2>Add VM</h2> 
+            <h2>Add VM</h2>
             <form onSubmit={handleAddVM}></form>
             <label>Name</label>
-            <input 
+            <input
                 type="text"
                 required
                 value={vm_name}
                 onChange={(e) => setName(e.target.value)}
             />
             <label>VM Name</label>
-            
-            <input 
+
+            <input
                 type="text"
                 required
                 value={vm_path}
                 onChange={(e) => setPath(e.target.value)}
             />
             <label>OS Name</label>
-            
-            <input 
+
+            <input
                 type="text"
                 required
                 value={vm_os}
                 onChange={(e) => setOs(e.target.value)}
             />
             <label>OS Name</label>
-            
-            <input 
+
+            <input
                 type="number" placeholder="xxx.xxx.xxx.xx"
                 required
                 value={vm_ip}
